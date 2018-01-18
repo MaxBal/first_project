394f9f91a4330122cb52b47d35fb2b27966e4b5c
Rob <talabishka22@mail.ru>
2018-01-18 20:59:46 +0200

    версия 2.0
    Шаблон

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button go_basket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton(){
+        go_basket = findViewById(R.id.go_basket);
+        go_basket.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent("com.example.myapplication.basketf");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
---
+++ app/src/main/java/com/example/myapplication/basketf.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class basketf extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basketf);
+
+
+
+
+    }
+
+}
